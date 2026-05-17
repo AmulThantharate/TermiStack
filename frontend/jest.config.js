e8f45f7529dbf803fcd8d477013ceffdb12b21b6
AmulThantharate <amulthantharate69@gmail.com>
2026-05-17 12:12:04 +0530

    chore: refactor jest.config.js to update formatting and disable require-imports linting rule

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,19 +1,16 @@
-const nextJest = require('next/jest');
+/* eslint-disable @typescript-eslint/no-require-imports */
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
+  dir: "./",
 });
 
-// Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
-    // Handle module aliases (e.g., @/lib/api -> src/lib/api)
-    '^@/(.*)$': '<rootDir>/src/$1',
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig);
